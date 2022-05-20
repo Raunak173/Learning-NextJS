e281fb9985a38783ae0aed38400c49635945354e
Raunak173 <raunakagarwal173@gmail.com>
2022-05-21 00:57:51 +0530

    created a basic skeleton of blog app

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,64 @@
+import Link from "next/link";
 import React from "react";
+import styles from "../styles/blog.module.css";
 
 const blog = () => {
-  return <div>Blog Page</div>;
+  return (
+    <div className={styles.con}>
+      <div>
+        <Link href="/blogposts/learn-js">
+          <h1>Post 1: Learn Javascript</h1>
+        </Link>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+          dolorum eius quos repellendus! Distinctio accusamus vel quis culpa hic
+          a corporis id, ducimus nihil voluptate? Animi ipsa voluptatum
+          praesentium repudiandae, mollitia illum ducimus porro dolore, minus
+          sit veniam eos sequi! Iure natus minus quidem culpa architecto facilis
+          nemo laboriosam qui?
+        </p>
+      </div>
+      <div>
+        <Link href="/blogposts/learn-ts">
+          <h1>Post 2: Learn Typescript</h1>
+        </Link>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+          dolorum eius quos repellendus! Distinctio accusamus vel quis culpa hic
+          a corporis id, ducimus nihil voluptate? Animi ipsa voluptatum
+          praesentium repudiandae, mollitia illum ducimus porro dolore, minus
+          sit veniam eos sequi! Iure natus minus quidem culpa architecto facilis
+          nemo laboriosam qui?
+        </p>
+      </div>
+      <div>
+        <Link href="/blogposts/learn-react">
+          <h1>Post 3: Learn React</h1>
+        </Link>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+          dolorum eius quos repellendus! Distinctio accusamus vel quis culpa hic
+          a corporis id, ducimus nihil voluptate? Animi ipsa voluptatum
+          praesentium repudiandae, mollitia illum ducimus porro dolore, minus
+          sit veniam eos sequi! Iure natus minus quidem culpa architecto facilis
+          nemo laboriosam qui?
+        </p>
+      </div>
+      <div>
+        <Link href="/blogposts/learn-c++">
+          <h1>Post 4: Learn C++</h1>
+        </Link>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+          dolorum eius quos repellendus! Distinctio accusamus vel quis culpa hic
+          a corporis id, ducimus nihil voluptate? Animi ipsa voluptatum
+          praesentium repudiandae, mollitia illum ducimus porro dolore, minus
+          sit veniam eos sequi! Iure natus minus quidem culpa architecto facilis
+          nemo laboriosam qui?
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default blog;
--- pages/blogposts/[slug].js
+++ pages/blogposts/[slug].js
@@ -1,11 +1,30 @@
 import React from "react";
 import { useRouter } from "next/router";
+import styles from "../../styles/slug.module.css";
 
 const slug = () => {
   const router = useRouter();
   console.log(router.query);
   const { slug } = router.query;
-  return <div>{slug}</div>;
+  return (
+    <div className={styles.con}>
+      <h1 className={styles.heading}>Title of the page: {slug}</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates
+        voluptatibus mollitia, officiis vitae soluta magnam porro cumque earum
+        possimus natus pariatur quos commodi maxime placeat, maiores architecto
+        tempora consectetur. Voluptatum nulla rem vero, officia libero, porro
+        temporibus veniam accusantium molestias, aliquam dolorem est ipsa enim
+        atque fugiat at laudantium quaerat neque sequi reprehenderit ab magnam
+        ipsam? Dignissimos nostrum sunt, rem quibusdam temporibus doloribus! Qui
+        aliquid veritatis dicta sunt eligendi iusto aut et pariatur facere,
+        tempora reprehenderit earum sequi, expedita illum, blanditiis error!
+        Architecto illum repudiandae assumenda aperiam ipsa temporibus numquam
+        corrupti officiis, suscipit unde quisquam in fugiat laboriosam
+        necessitatibus error?
+      </p>
+    </div>
+  );
 };
 
 export default slug;
