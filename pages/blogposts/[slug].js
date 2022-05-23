a5f0f8c07025f9280bf62eef74924e422db31531
Raunak173 <raunakagarwal173@gmail.com>
2022-05-23 13:45:54 +0530

    blogzo blogs populated

--- pages/blogposts/[slug].js
+++ pages/blogposts/[slug].js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import styles from "../../styles/slug.module.css";
 
 const slug = () => {
+  const [blog, setBlog] = useState();
   const router = useRouter();
-  console.log(router.query);
-  const { slug } = router.query;
+  useEffect(() => {
+    if (!router.isReady) return;
+    const { slug } = router.query;
+    fetch(`http://localhost:3000/api/getBlog?slug=${slug}`)
+      .then((a) => {
+        return a.json();
+      })
+      .then((parsed) => {
+        console.log(parsed);
+        setBlog(parsed);
+      });
+  }, [router.isReady]);
+
   return (
     <div className={styles.con}>
-      <h1 className={styles.heading}>Title of the page: {slug}</h1>
+      <h1 className={styles.heading}>{blog && blog.title}</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates
+        {/* Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates
         voluptatibus mollitia, officiis vitae soluta magnam porro cumque earum
         possimus natus pariatur quos commodi maxime placeat, maiores architecto
         tempora consectetur. Voluptatum nulla rem vero, officia libero, porro
@@ -21,7 +33,8 @@ const slug = () => {
         tempora reprehenderit earum sequi, expedita illum, blanditiis error!
         Architecto illum repudiandae assumenda aperiam ipsa temporibus numquam
         corrupti officiis, suscipit unde quisquam in fugiat laboriosam
-        necessitatibus error?
+        necessitatibus error? */}
+        {blog && blog.content}
       </p>
     </div>
   );
